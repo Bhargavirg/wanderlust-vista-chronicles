c444f585ebef81129fc4f0203e141c4b43f84457
Bhargavirg <bhargavigothe563@gmail.com>
2025-04-22 08:52:09 +0530

    changed

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -7,7 +7,7 @@ import { mockData, BlogPost } from "../data/blogData";
 import { Atom, Rocket, History, Globe, Leaf, BookOpen, Camera } from "lucide-react";
 import { motion } from "framer-motion";
 import VideoEmbed from "../components/blog/VideoEmbed";
-import "../pages/categoryPage.css";
+import "./categoryPage.css";
 
 const CategoryPage = () => {
   const { category } = useParams<{ category: string }>();
